9fb76a4b5586c4f54c86f60db7f225f9f383c1cd
ilyass atlassi <atlassiilyass@gmail.com>
2023-02-22 15:02:17 +0100

    Add show list function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
-  
-    return element;
+const list = document.getElementById('listtodo');
+const add = document.getElementById('add');
+console.log(add)
+
+let todoList = [
+  {
+    index:0,
+    description:"wash the dishes",
+    completed: false
+  },
+  {
+    index:1,
+    description:"wash the dishes",
+    completed: false
   }
-  
-  document.body.appendChild(component());
+];
+
+const showList = () => {
+  let listItems = "";
+  todoList.forEach((element) => {
+    listItems += `<li class="task">
+                                  <div>
+                                  <input type="checkbox" class="checkFunc"  id="check${element.index}">
+                                  <input type="text" value="${element.description}" class="listitem" id="item${element.index}" readonly>
+                                  </div>
+                                  <div class = "tools">
+                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}"></i>
+                                  <i class="fa-solid fa-floppy-disk save hide box" id="save${element.index}"></i>
+                                  <i id="removeicon"  class="fa-solid fa-trash"></i>
+                                  </div>
+                          </li>`
+  })
+  list.innerHTML = listItems;
+  add.value = ''
+}
+
+showList()
