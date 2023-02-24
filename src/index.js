9a4b3d4cd92a71dd00b49c1ad90ad65d17fad622
ilyass atlassi <atlassiilyass@gmail.com>
2023-02-25 00:03:38 +0100

    Update the delete function

--- src/index.js
+++ src/index.js
@@ -1,55 +1,73 @@
+/* eslint-disable  */
 import './style.css';
 
 const list = document.getElementById('listtodo');
 const add = document.getElementById('add');
 
+let completed = false;
+let index = 0;
 let todoList = [];
 
 const showList = () => {
-  let listItems = '';
+  let litItems = "";
   todoList.forEach((element) => {
-    listItems += `<li class="task">
+  if(element.completed === true) {
+    litItems += `<li class="task">
                                   <div>
-                                  <input type="checkbox" class="checkFunc"  id="check${element.index}">
+                                  <input type="checkbox" class="checkFunc" checked onclick="checkFunc(${element.index});" id="check${element.index}">
                                   <input type="text" value="${element.description}" class="listitem" id="item${element.index}" readonly>
                                   </div>
                                   <div class = "tools">
-                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}"></i>
-                                  <i class="fa-solid fa-floppy-disk save hide box" id="save${element.index}"></i>
-                                  <i id="removeicon"  onclick="removeFunc(${element.index});" class="fa-solid fa-trash"></i>
+                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}" onclick="editFunc(${element.index});"></i>
+                                  <i class="fa-solid fa-floppy-disk save hide box" id="save${element.index}" onclick="saveFunc(${element.index});"></i>
+                                  <i id="removeicon" onclick="removeFunc(${element.index});" class="fa-solid fa-trash"></i>
                                   </div>
-                          </li>`;
-  });
-  list.innerHTML = listItems;
+                          </li>`
+  }
+  else {
+    litItems += `<li class="task">
+                                  <div>
+                                  <input type="checkbox" class="checkFunc" onclick="checkFunc(${element.index});" id="check${element.index}">
+                                  <input type="text" value="${element.description}" class="listitem" id="item${element.index}" readonly>
+                                  </div>
+                                  <div class = "tools">
+                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}" onclick="editFunc(${element.index});"></i>
+                                  <i class="fa-solid fa-floppy-disk save hide box" id="save${element.index}" onclick="saveFunc(${element.index});"></i>
+                                  <i id="removeicon" onclick="removeFunc(${element.index});" class="fa-solid fa-trash"></i>
+                                  </div>
+                          </li>`
+
+  }
+});
+
+  list.innerHTML = litItems;
   add.value = ''
-};
+}
 
 showList();
 
-
-// Adding function
-add.addEventListener("keypress",(e) => {
+add.addEventListener("keypress", (e) => {
   if (e.key === "Enter" && add.value.length != 0) {
-    let storedList = localStorage.getItem("todo");
+      let storedList = localStorage.getItem("todo");
 
-  if (storedList === null) {
-      todoList = [];
-  } else {
-      todoList = JSON.parse(storedList);
+      if (storedList === null) {
+          todoList = [];
+      } else {
+          todoList = JSON.parse(storedList);
+          index = todoList.length === 0 ? 0 : todoList.length;
+      }
+
+      const LocalStore = {
+          index: index,
+          description: add.value,
+          completed: completed,
+      };
+      todoList.push(LocalStore);
+      localStorage.setItem("todo", JSON.stringify(todoList));
+      showList();
   }
+});
 
-  const LocalStore = {
-    index: Date.now(),
-    description: add.value,
-    completed: false,
-  };
-  todoList.push(LocalStore);
-  localStorage.setItem("todo", JSON.stringify(todoList));
-  showList();
-  }
-})
-
-// call the data from the locale storage
 
 window.onload = () => {
   if (localStorage.getItem("todo")) {
@@ -57,17 +75,14 @@ window.onload = () => {
   }
   showList()
 };
-
-// Remove function
-
+  
 window.removeFunc = (index) => {
   let storedData = localStorage.getItem("todo");
   todoList = JSON.parse(storedData);
-  console.log(todoList)
-  
   todoList.splice(index, 1);
-  console.log(index)
-
+  for (let i = 0; i < todoList.length; i++) {
+      todoList[i].index = i;
+  }
   localStorage.setItem("todo", JSON.stringify(todoList));
   showList()
 };
