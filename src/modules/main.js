0b657e184e562754a464ae9b8751f4556ce506c3
ilyass atlassi <atlassiilyass@gmail.com>
2023-02-25 20:12:55 +0100

    Add required change 2

--- src/modules/main.js
+++ src/modules/main.js
@@ -2,7 +2,7 @@ const list = document.getElementById('listtodo');
 const add = document.getElementById('add');
 
 const completed = false;
-let index = 0;
+let index = 1;
 let todoList = [];
 
 const showList = () => {
@@ -11,25 +11,25 @@ const showList = () => {
     if (element.completed === true) {
       litItems += `<li class="task">
                                   <div>
-                                  <input type="checkbox" class="checkFunc" checked onclick="checkFunc(${element.index});" id="check${element.index}">
-                                  <input type="text" value="${element.description}" class="listitem" id="item${element.index}" readonly>
+                                  <input type="checkbox" class="checkFunc" checked onclick="checkFunc(${element.index -1});" id="check${element.index -1}">
+                                  <input type="text" value="${element.description}" class="listitem" id="item${element.index -1}" readonly>
                                   </div>
                                   <div class = "tools">
-                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}" onclick="editFunc(${element.index});"></i>
-                                  <i class="fa-solid fa-floppy-disk save hide box" id="save${element.index}" onclick="saveFunc(${element.index});"></i>
-                                  <i id="removeicon" onclick="removeFunc(${element.index});" class="fa-solid fa-trash"></i>
+                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index -1}" onclick="editFunc(${element.index -1});"></i>
+                                  <i class="fa-solid fa-floppy-disk save hide box" id="save${element.index -1}" onclick="saveFunc(${element.index -1});"></i>
+                                  <i id="removeicon" onclick="removeFunc(${element.index-1});" class="fa-solid fa-trash"></i>
                                   </div>
                           </li>`;
     } else {
       litItems += `<li class="task">
                                   <div>
-                                  <input type="checkbox" class="checkFunc" onclick="checkFunc(${element.index});" id="check${element.index}">
-                                  <input type="text" value="${element.description}" class="listitem" id="item${element.index}" readonly>
+                                  <input type="checkbox" class="checkFunc" onclick="checkFunc(${element.index -1});" id="check${element.index -1}">
+                                  <input type="text" value="${element.description}" class="listitem" id="item${element.index -1}" readonly>
                                   </div>
                                   <div class = "tools">
-                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}" onclick="editFunc(${element.index});"></i>
-                                  <i class="fa-solid fa-floppy-disk save hide box" id="save${element.index}" onclick="saveFunc(${element.index});"></i>
-                                  <i id="removeicon" onclick="removeFunc(${element.index});" class="fa-solid fa-trash"></i>
+                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index -1}" onclick="editFunc(${element.index -1});"></i>
+                                  <i class="fa-solid fa-floppy-disk save hide box" id="save${element.index -1}" onclick="saveFunc(${element.index -1});"></i>
+                                  <i id="removeicon" onclick="removeFunc(${element.index-1});" class="fa-solid fa-trash"></i>
                                   </div>
                           </li>`;
     }
@@ -38,18 +38,20 @@ const showList = () => {
   list.innerHTML = litItems;
   add.value = '';
 };
-
+console.log(localStorage.getItem('todo'))
 showList();
 // Add function
 add.addEventListener('keypress', (e) => {
   if (e.key === 'Enter' && add.value.length !== 0) {
     const storedList = localStorage.getItem('todo');
+    
+    console.log(storedList)
 
     if (storedList === null) {
       todoList = [];
     } else {
       todoList = JSON.parse(storedList);
-      index = todoList.length === 0 ? 0 : todoList.length;
+      index = todoList.length === 0 ? 1 : todoList.length +1 ;
     }
 
     const LocalStore = {
@@ -59,6 +61,7 @@ add.addEventListener('keypress', (e) => {
     };
     todoList.push(LocalStore);
     localStorage.setItem('todo', JSON.stringify(todoList));
+    // index += 1;
     showList();
   }
 });
@@ -99,7 +102,7 @@ window.saveFunc = (index) => {
   todoList = JSON.parse(storedData);
   todoList[index].description = mainItem.value;
 
-  localStorage.setItem('todo-list', JSON.stringify(todoList));
+  localStorage.setItem('todo', JSON.stringify(todoList));
   showList();
 };
 
@@ -108,11 +111,12 @@ window.removeFunc = (index) => {
   const storedData = localStorage.getItem('todo');
   todoList = JSON.parse(storedData);
   todoList.splice(index, 1);
-  for (let i = 0; i < todoList.length; i += 1) {
-    todoList[i].index = i;
+  for (let i = 0; i < todoList.length ; i += 1) {
+    todoList[i].index = i+1;
   }
   localStorage.setItem('todo', JSON.stringify(todoList));
   showList();
+console.log(todoList)
 };
 
 // Check function
@@ -141,8 +145,8 @@ clean.addEventListener('click', () => {
   todoList = JSON.parse(storedData);
   const clearDone = todoList.filter((element) => element.completed === false);
   todoList = clearDone;
-  for (let i = 0; i < todoList.length; i = +1) {
-    todoList[i].index = i;
+  for (let i = 0; i < todoList.length; i +=1) {
+    todoList[i].index = i+1;
   }
   localStorage.setItem('todo', JSON.stringify(todoList));
   showList();
